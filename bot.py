b9f9bb7a707041cb04422928fd1ae2d7a28c1f2e
EvgeniyKondrashyn <e.kondrashyn@gmail.com>
2017-11-20 01:14:01 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+from db_sqlite import DBsqlite
+from telegram import Bot, Update, Message
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+import logging
+from config import *
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import CallbackQuery
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# define sqlite setup statements
+sqlitesetupstatements = "PRAGMA foreign_keys = ON;"
+db = DBsqlite(database, sqlitesetupstatements)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+
+def button_callback(bot, update):
+    query = update.callback_query
+    message = query.message
+    res = db.rate(query)
+#    if res:
+#        original_msg = db.original_message(query)
+#        reply_markup = get_buttons_markup(original_msg, res)
+#        bot.edit_message_reply_markup(chat_id=message.chat_id, message_id=message.message_id, reply_markup=reply_markup)
+
+def get_buttons_markup():
+    keys = []
+    keyboard = []
+    max_cols = 4
+    buttons = db.get_keyboard()
+    for button in buttons:
+        text = chr(button[0])
+        name = button[1]
+        keys.append(InlineKeyboardButton(text, callback_data=name))
+    while keys:
+        keyboard += [keys[:max_cols]]
+        keys = keys[max_cols:]
+    return InlineKeyboardMarkup(keyboard)
+
+def start(bot, update):
+    update.message.reply_text("Bot has started\n" + update.message.status_update.new_chat_members)
+
+def joined(bot, update):
+    bot.send_message(update.message.chat_id,'I am joined' + str(update.message.new_chat_members))
+    print(*update.message.new_chat_members)
+    db.register_chat([update.message.chat.id, update.message.chat.title, update.message.chat.username, update.message.chat.description])
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"'.format(update, error))
+
+def resend_message(bot, update):
+    resend = True
+    message = update.message
+    if message.reply_to_message:
+        return
+    if message.photo:
+        bot.send_photo(chat_id = message.chat_id, photo=message.photo[0].file_id, disable_notification = True, reply_markup=reply_markup)
+    else:
+        resend = False
+    if resend:
+        message.delete()
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(token)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # We joined a chat
+    # 
+    print(db.get_keyboard())
+    get_buttons_markup()
+    global reply_markup
+    reply_markup = get_buttons_markup()
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_error_handler(error)
+    msghandler = MessageHandler(Filters.all, resend_message)
+    new_members_handler = MessageHandler(Filters.status_update.new_chat_members, joined)
+    dp.add_handler(new_members_handler)
+    dp.add_handler(msghandler)
+    dp.add_handler(CallbackQueryHandler(button_callback))
+    # To be replaced with Webhook method
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ db_sqlite.py
@@ -0,0 +1,131 @@
+import sqlite3
+"""
+A class for interaction with SQLITE DB
+
+"""
+
+
+class DBsqlite(object):    
+
+    def __init__(self, database='database.db', statements=None):
+        self.database = database
+        if statements is None:
+            statements = []
+        self.statement = ''
+        self.display = False
+        self.connect()
+        self.execute(statements)
+        self.close()            
+
+    def connect(self):
+        self.connection = sqlite3.connect(self.database)
+        #self.connection.row_factory = lambda cursor, row: row[1]
+        self.cursor = self.connection.cursor()
+        self.connected = True
+
+    def close(self): 
+        self.connection.commit()
+        self.connection.close()
+        self.connected = False
+
+    def rate(self, query):
+        close = False
+        if not self.connected:
+            self.connect()
+            close = True
+        chat_id = query.message.chat_id
+        message_id = query.message.message_id
+        user_id = query.from_user.id
+        chosen_emoji = query.data
+        print(query.from_user)
+        self.add_user(query.from_user)
+        
+        try:
+
+            # get reactions for message
+            self.cursor.execute("SELECT tmsg_id, description, result FROM rates JOIN reactions ON rates.reaction_id = reactions.id \
+				JOIN messages ON message_id = messages.id \
+				WHERE messages.tmsg_id = ? AND messages.chat_id = ?", (message_id, chat_id))
+            reactions = self.cursor.fetchall()
+        except sqlite3.Error as error:
+            print ('An error occurred:', error.args[0])
+        
+            # TODO:
+            # increment count in rates table for chosen reaction using description
+            # add user to voters table
+        if close:
+            self.close()
+
+    def add_user(self, user):
+        close = False
+        if not self.connected:
+            self.connect()
+            close = True
+        try:
+            self.cursor.execute("INSERT INTO users (id, nickname, fname, lname) values (?, ?, ?, ?)",
+					(user.id, user.username, user.first_name, user.last_name)).fetchall()
+        except sqlite3.Error as error:
+            print ('An error occurred:', error.args[0])
+        # close connection if one was opened
+        if close:
+            self.close()
+
+    def get_keyboard(self):
+        close = False
+        if not self.connected:
+            self.connect()
+            close = True
+        try:
+            data = self.cursor.execute('SELECT value, description FROM reactions;').fetchall()
+        except sqlite3.Error as error:
+            print ('An error occurred:', error.args[0])
+        # close connection if one was opened
+        if close:
+            self.close()   
+        return data 
+
+    def register_chat(self, chat_values):
+        if not self.connected:
+            self.connect()
+            close = True
+        try:
+            self.cursor.execute('INSERT INTO chats (id, name, nickname, description) values (?, ?, ?, ?);', chat_values)
+        except sqlite3.Error as error:
+            print ('An error occurred:', error.args[0])
+        # close connection if one was opened
+        if close:
+            self.close()   
+
+    def execute(self, statement, args=None):
+        queries = []
+        close = False
+        if not self.connected:
+            self.connect()
+            close = True
+        try:
+            print(statement)
+            if args:
+                self.cursor.execute(statement, *args)
+            else:
+                self.cursor.execute(statement)
+            #retrieve selected data
+            data = self.cursor.fetchone()
+            if statement.upper().startswith('SELECT'):
+                #append query results
+                queries.append(data)
+
+        except sqlite3.Error as error:
+            print ('An error occurred:', error.args[0])
+            print ('For the statement:', statement)
+        # close connection if one was opened
+        if close:
+            self.close()   
+        if self.display:      
+            for result in queries:
+                if result:
+                    for row in result:
+                        print (row)
+                else:
+                    print (result)
+        else:
+            return queries
